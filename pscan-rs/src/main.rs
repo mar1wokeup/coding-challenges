918e40bcc2166aa4892a346f15479a9321b3b954
mar1 <marwanechampain@icloud.com>
2024-01-20 18:22:23 +0100

    add host and port param parsing

--- pscan-rs/src/main.rs
+++ pscan-rs/src/main.rs
@@ -7,22 +7,30 @@ use std::sync::mpsc::channel;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    let all_ports: Vec<u16> = (1..=65535).collect();
+
     if args.len() < 2 {
         eprintln!(
-            "Usage: pscan-rs -host=<hostname> [-timeout=<milliseconds>] [-threads=<num_threads>]"
+            "Usage: pscan-rs -hosts=<hostname> -port=<port> (empty is full scan) [-timeout=<milliseconds>] [-threads=<num_threads>]"
         );
         std::process::exit(1);
     }
 
-    let host = &args[1][6..];
+    let host = parse_hosts(&args[1][7..]);
+
+    let mut ports = parse_ports(&args[2][6..]);
+    if ports.is_empty() {
+        ports = all_ports;
+    }
+
     let timeout = args
-        .get(2)
+        .get(3)
         .and_then(|arg| arg.strip_prefix("-timeout="))
         .and_then(|t| t.parse().ok())
         .unwrap_or(500); // default t.o. 500 ms
 
     let num_threads = args
-        .get(3)
+        .get(4)
         .and_then(|arg| arg.strip_prefix("-threads="))
         .and_then(|t| t.parse().ok())
         .unwrap_or(100); // default 100 threads
@@ -30,36 +38,77 @@ fn main() {
     let pool = ThreadPool::new(num_threads);
     let (tx, rx) = channel();
 
-    println!("Scanning host: {}", host);
+    // println!("Scanning host: {}", host);
 
-    for port in 1..=65535 {
+    for host in host {
         let tx = tx.clone();
-        let host = host.to_string();
-        pool.execute(move || {
-            let addr = format!("{}:{}", host, port);
 
-            match addr.to_socket_addrs() {
-                Ok(mut addrs) => {
-                    if let Some(addr) = addrs.next() {
-                        if is_port_open(addr, timeout) {
-                            tx.send(port).unwrap_or_else(|e| {
-                                eprintln!("Failed to send port: {}", e);
-                            });
+        // println!("Scanning host: {}", host);
+        // let host = host.to_string();
+        let p = ports.clone();
+        let h = host.clone();
+
+        pool.execute(move || {
+            for port in p {
+                println!("Scanning port: {} on host {}", port, host);
+                let addr = format!("{}:{}", h, port);
+                match addr.to_socket_addrs() {
+                    Ok(mut addrs) => {
+                        if let Some(addr) = addrs.next() {
+                            if is_port_open(addr, timeout) {
+                                tx.send((port, h.clone())).unwrap_or_else(|e| {
+                                    eprintln!("Failed to send port: {}", e);
+                                });
+                            }
                         }
                     }
+                    Err(e) => eprintln!("Failed to resolve address for {}: {}", addr, e),
                 }
-                Err(e) => eprintln!("Failed to resolve address for {}: {}", addr, e),
             }
         });
     }
 
     drop(tx); // close the channel
 
-    for port in rx.iter() {
-        println!("Port: {} is open", port);
+    for (port, host) in rx.iter() {
+        println!(" +++ Port: {} is open on host: {}", port, host);
     }
 
     println!("\nDone")
+}
+
+fn parse_hosts(hosts: &str) -> Vec<String> {
+    if hosts.contains("*") {
+        expand_wildcard(hosts)
+    } else {
+        hosts.split(',').map(String::from).collect()
+    }
+}
+
+fn expand_wildcard(host: &str) -> Vec<String> {
+    let mut expanded_hosts = Vec::new();
+    let base_ip = host.replace("*", "");
+    for i in 1..=255 {
+        expanded_hosts.push(format!("{}{}", base_ip, i));
+    }
+    expanded_hosts
+}
+
+fn parse_ports(port_arg: &str) -> Vec<u16> {
+    let mut ports = Vec::new();
+    for part in port_arg.split(',') {
+        if part.contains(':') {
+            let range: Vec<&str> = part.split(':').collect();
+            if range.len() == 2 {
+                if let (Ok(start), Ok(end)) = (range[0].parse::<u16>(), range[1].parse::<u16>()) {
+                    ports.extend(start..=end);
+                }
+            }
+        } else if let Ok(port) = part.parse::<u16>() {
+            ports.push(port);
+        }
+    }
+    ports
 }
 
 fn is_port_open(addr: SocketAddr, timeout_ms: u64) -> bool {
